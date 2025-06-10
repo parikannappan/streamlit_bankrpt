e1bd45c005d9c85ebe5f7869eca75cb250df3042
pari kannappan <parikannappan@gmail.com>
2025-06-10 15:53:18 +0530

    Update mitostreamlit.py

--- mitostreamlit.py
+++ mitostreamlit.py
@@ -4,7 +4,7 @@ from mitosheet.streamlit.v1 import spreadsheet
 st.set_page_config(layout="wide")
 st.title('Tesla Stock Volume Analysis')
 
-CSV_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv'
+CSV_URL = 'https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/2014_usa_states.csv'
 new_dfs, code = spreadsheet(CSV_URL)
 
 st.write(new_dfs)
