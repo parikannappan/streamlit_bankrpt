071c37f7a6d1880074b7fb5ce8dd04446d0fd22a
pari kannappan <parikannappan@gmail.com>
2025-06-30 21:35:50 +0530

    Update icicbankrpt.py

--- icicbankrpt.py
+++ icicbankrpt.py
@@ -30,7 +30,7 @@ if fl is not None:
     Bankdata = Bankdata.drop(['Unnamed: 0','Cheque Number'], axis=1)
     Bankdata['Transaction Remarks'].fillna('', inplace=True)
     with st.expander("View All Data"):
-       st.dataframe(Bankdata[['Value Date','Withdrawals', 'Deposits','Transaction Remarks']])   
+       st.text(Bankdata[['Value Date','Withdrawals', 'Deposits','Transaction Remarks']])   
     top_5_withdrawals = Bankdata.sort_values(by='Withdrawals', ascending=False).head(6)  
     #top_5_withdrawals = top_5_withdrawals.reset_index(drop=True)
     #top_5_wthdropt  = top_5_withdrawals.drop(index=0, axis=0, inplace=False) #drop Totals row
@@ -38,10 +38,35 @@ if fl is not None:
     #top_5_deposits = top_5_deposits.reset_index(drop=True)
     #top_5_depdropt  = top_5_deposits.drop(index=0, axis=0, inplace=False)
     with st.expander("Top 5 Withdrawls"):
-       st.dataframe(top_5_withdrawals)   
+       st.text(top_5_withdrawals)   
     with st.expander("Top 5 Deposits"):  
-       st.dataframe(top_5_deposits)
+       st.text(top_5_deposits)
     stinput = st.text_input("Enter keyword to search -")
+    #
+    word_list = Bankdata['Transaction Remarks'].str.split().explode().tolist()
+       #print(f'word_list', word_list)
+    word_list = [str(word) for word in word_list] 
+    new_word_list = []
+    for word in word_list:
+          if isinstance(word, str) and '/' in word:
+            new_word_list.extend(word.split('/'))
+          else:
+            new_word_list.append(word)
+    new_word_list = [word1 for word1 in new_word_list if not word1.endswith(',')]
+    words_set = set(new_word_list)
+    few_trans_all = list(words_set)
+    mischr_list = ['IN','OF','of','FROM', '+', 'ORG','from', 'To','R','N',
+                      '-','Ref:','NO', 'in','F','B']
+    few_trans = [item for item in few_trans_all if item not in mischr_list ]
+       #combined_set = set().union(*pt_set_list)
+    col1, col2 = st.columns(2)
+    with col1:
+        selected_key =   st.multiselect(f' "you can choose keywords for  " ', few_trans) 
+    st.text(selected_key)
+    choices = '|'.join(selected_key)  # Regex OR pattern
+    choice_data = Bankdata[Bankdata['Transaction Remarks'].str.contains(choices, case=False, regex=True)]
+    if len(selected_key) > 0:
+      st.text(choice_data)
     #
     if len(stinput) > 0:
        print('stinput-1', stinput)
@@ -57,10 +82,14 @@ if fl is not None:
        st.write(f':money_with_wings: :red[WITHDRAWALS  -:  {dfs1w}]   :moneybag: :green[DEPOSITS   -:  {dfs1d}  ]')
        st.write(f':abacus: With - Dep = {sumdif}                 ')
        with st.expander("View Transactions"):
-          st.dataframe(dfs1[['Value Date','Transaction', 'Withdrawals', 'Deposits','Transaction Remarks']])    
+          st.text(dfs1[['Value Date','Transaction', 'Withdrawals', 'Deposits','Transaction Remarks']]) 
+       dfs2 = dfs1[['Value Date','Transaction', 'Withdrawals', 'Deposits','Transaction Remarks']]     
+       
+    
     else:
         print('stinput-2 ', stinput)
         dfs1d = 0
         dfs1w = 0
          
+            
    
